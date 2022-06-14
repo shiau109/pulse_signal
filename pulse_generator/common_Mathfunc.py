a445661b44e505e89326049bd39837773cf05b81
shiau109 <shiau109@gmail.com>
2022-06-14 16:14:54 +0800

    Add new and fix math function
    Add rectPulseFunc
    
    TODO GERPfunc

--- pulse_generator/common_Mathfunc.py
+++ pulse_generator/common_Mathfunc.py
@@ -2,11 +2,11 @@
 # Typing
 from numpy import ndarray
 # Numpy array
-from numpy import array, append, zeros, ones, where, linspace, arange
+from numpy import array, append, zeros, ones, where, linspace, arange, searchsorted
 # Numpy common math function
 from numpy import exp, sqrt, arctan2, cos, sin, angle, radians, sign, log, ceil
 # Numpy constant
-from numpy import pi
+from numpy import pi, logical_and
 
 
 
@@ -35,14 +35,53 @@ def derivativeGaussianFunc (x, *p)->ndarray:
     else :
         return zeros(len(x))
 
-def constFunc (t, *p)->ndarray:
+def constFunc (x, *p)->ndarray:
     """
     return constant array
     x: array like, shape (n,) \n
     p[0]: value \n
     """
-    return p[0]*ones(len(t))
+    return p[0]*ones(len(x))
 
+def rectPulseFunc (x, *p)->ndarray:
+    """
+    return constant array
+    x: array like, shape (n,) \n
+    p[0]: amp \n
+    p[1]: width \n
+    p[2]: start \n
+    """
+    condition = logical_and(abs(x)>=p[2], abs(x)<=(p[1]+p[2]))
+    #print(condition)
+    return where(condition, p[0], 0)
+
+def GERP (x, *p)->ndarray:
+    """
+    return Gaussian Edge Rectangular Pulse array
+    x: array like, shape (n,) \n
+    p[0]: amp \n
+    p[1]: width \n
+    p[2]: start \n
+    p[3]: edge width \n
+    p[4]: edge sigma \n
+    """
+    amp = p[0]
+    width = p[1]
+    edgeLength = p[3]
+    flat_start = p[2]
+    peakLength = edgeLength*2
+    flatLength = x -peakLength
+    peakSigma = peakLength
+
+    startPos = edgeLength
+    endPos = startPos +flatLength
+
+    startEdge = [ p[0], p[4], startPos ]
+    gaussUp = where( relativeTime<startPos, gaussianFunc(x, startEdge),0. )
+    endEdge = [ flatHieght, peakSigma, endPos ]
+    gaussDn = where( relativeTime>endPos, gaussianFunc(relativeTime, endEdge),0. )
+    step = where( (relativeTime>=startPos) & (relativeTime<=endPos), constFunc(relativeTime, [flatHieght]),0. )
+    return gaussUp +step +gaussDn
 def linearFunc (t, *p)->ndarray:
     """
     return constant array
@@ -52,9 +91,23 @@ def linearFunc (t, *p)->ndarray:
     """
     return p[0]*t+p[1]
 
+def DRAGFunc ( t, *p )->ndarray:
+    """
+    return gaussian +1j*derivative Gaussian\n
+    x: array like, shape (n,), the element is complex number \n
+    p[0]: amp \n
+    p[1]: sigma \n
+    p[2]: peak position \n
+    p[3]: derivative Gaussian amplitude ratio \n
+    """
+    gaussParas = (p[0],p[1],p[2])
+    return gaussianFunc( t, *gaussParas )+ 1j*p[3]*derivativeGaussianFunc( t, *gaussParas )
 
 if __name__ == '__main__':
     from numpy import linspace
-    x = linspace(0,10,10)
-    p = (0,1,2)
-    print(gaussianFunc(x,*p))
+    import matplotlib.pyplot as plt
+
+    x = linspace(0,9,100)
+    p = (1,3,0)
+    plt.plot(x,derivativeGaussianFunc(x,*p))
+    plt.show()
--- pulse_generator/pulse.py
+++ pulse_generator/pulse.py
@@ -2,11 +2,15 @@
 # sys.path.insert(0, r'../')
 # Numpy Series
 # Typing
-from numpy import ndarray
+from numpy import ndarray, complex128, issubdtype
+# Array
+from numpy import array, linspace, empty, append
+# Math
+from numpy import cos, sin, exp, arctan2, radians, sign
+# const
+from numpy import pi
 
-from numpy import array, linspace, empty
-
-from common_Mathfunc import gaussianFunc
+from pulse_generator.common_Mathfunc import gaussianFunc, DRAGFunc
 class Waveform():
     """ Data format for the f(x) with linear x input"""
     def __init__( self, x0:float=0, dx:float=1, Y:ndarray=array([]) ):
@@ -37,17 +41,22 @@ class Waveform():
         self._dx = value
 
     @property
-    def points ( self )->int:
+    def points( self )->int:
         """ Array length of Y """
         return self._points
+
+    def append( self, appended ):
+        if appended.dx == self.dx:
+            self.Y = append(self.Y, appended.Y)
+        else:
+            raise ValueError("dx are different")
 
     def get_xAxis ( self ):
         return linspace(self.x0, self.x0+self.dx*self.points,self.points, endpoint=False)
 
 
-class PulseBuilder():
+class Pulse():
     """ Store the necessary information for waveform """
-    modeList = ["Direct,Mixer"]
     def __init__ ( self ):
 
         self._duration = None
@@ -56,27 +65,6 @@ class PulseBuilder():
         self._envelopeFunc = None
         self._parameters = None
 
-    @property
-    def signal ( self )->Waveform:
-        """ The output waveform """
-        return self._signal
-    @signal.setter
-    def signal ( self, value ):
-        if isinstance(value, Waveform):
-            self._signal = value
-        elif isinstance(value, ndarray):
-            self._signal.Y = value
-
-    @property
-    def envelope ( self )->Waveform:
-        """ The envelope of the signal."""
-        return self._envelope
-    @envelope.setter
-    def envelope ( self, value ):
-        if isinstance(value, Waveform):
-            self._envelope = value
-        elif isinstance(value, ndarray):
-            self._envelope.Y = value
 
 
     @property
@@ -90,7 +78,7 @@ class PulseBuilder():
     @property
     def carrierPhase ( self )->float:
         """ The carrier phase of the signal, unit depended on dt."""
-        return self._carrierFrequency
+        return self._carrierPhase
     @carrierPhase.setter
     def carrierPhase ( self, value:float ):
         self._carrierPhase = value
@@ -119,39 +107,153 @@ class PulseBuilder():
     def parameters ( self, value:tuple ):
         self._parameters = value
 
-    def generate_envelope( self, t0:float, dt:float ):
-        """ For a given dt and t0, calculate the waveform"""
+    def generate_envelope( self, t0:float, dt:float )->Waveform:
+        """ For a given dt and t0, calculate the envelop waveform"""
         points = int( -(self.duration //-dt) )
-        self.envelope = Waveform(t0, dt, empty(points))
+        envelope = Waveform(t0, dt, empty(points))
 
-        time = self.envelope.get_xAxis()
-        self.envelope.Y = self.envelopeFunc( time, *self.parameters )
+        time = envelope.get_xAxis()
+        envelope.Y = self.envelopeFunc( time, *self.parameters )
         
+        return envelope
 
+    def generate_signal( self, t0:float, dt:float )->Waveform:
+        """ For a given dt and t0, calculate the signal waveform"""
+
+        envelope = self.generate_envelope( t0, dt )
+        signal = Waveform(envelope.x0, envelope.dx, empty(envelope.Y.shape[-1]))
+        time = envelope.get_xAxis()
+        if issubdtype(envelope.Y.dtype,complex):
+            phase_I = 2.*pi*self.carrierFrequency*time +self.carrierPhase
+            print(time[0])
+            phase_Q = phase_I +pi/2
+            LO_I = cos( phase_I )
+            LO_Q = cos( phase_Q )
+            signal.Y = envelope.Y.real*LO_I +envelope.Y.imag*LO_Q
+        else:
+            signal.Y = envelope.Y*cos( 2.*pi*self.carrierFrequency*time +self.carrierPhase)
+
+        return signal
+
+    def generate_IQSignal( self, t0:float, dt:float, IFFreq:float, IQMixer:tuple=(1,90,0,0) ):
+        """
+        For a given dt and t0, calculate the output for IQMixer 
+        to form the target signal waveform.\n
+
+        IFFreq: The Intermediate frequency of I/Q ( Unit in dt ) \n
+        IQMixer: The parametrs for calibrate IQmixer\n
+            p1: I/Q Amplitude Balance ( dimensionless ratio )\n
+            p2: Phase Balance ( unit in angle )\n
+            p3: I offset\n
+            p4: Q offset\n
+        The LO frequency should be RF+IF (RF is carrier frequency)
+        """
+        envelope = self.generate_envelope( t0, dt )
+        time = envelope.get_xAxis()
+
+        Signal_I = Waveform(envelope.x0, envelope.dx, empty(envelope.Y.shape[-1]))
+        Signal_Q = Waveform(envelope.x0, envelope.dx, empty(envelope.Y.shape[-1]))
+        
+        ampBalance = IQMixer[0]
+        phaseBalance = IQMixer[1]
+        offsetI = IQMixer[2]
+        offsetQ = IQMixer[3]
+        LOShiftSign = sign(sin(radians(phaseBalance)))
+
+        envelopeIQ = abs( envelope.Y )
+        envelopeI = envelopeIQ /cos(radians(abs(phaseBalance)-90))
+        envelopeQ = envelopeI /ampBalance
+        phi_envelope = arctan2( envelope.Y.imag, envelope.Y.real )
+        phi_Q = phi_envelope -LOShiftSign*pi/2 +self.carrierPhase
+        phi_I = phi_Q -radians(phaseBalance) +pi
+        Signal_I.Y = envelopeI *cos( 2. *pi *IFFreq *time +phi_I) -offsetI
+        Signal_Q.Y = envelopeQ *cos( 2. *pi *IFFreq *time +phi_Q) -offsetQ
+
+        return Signal_I, Signal_Q
+def simulate_IQMixer ( I:Waveform, Q:Waveform, LOFreq:float, IQMixer:tuple=(1,90,0,0)):
+    time = I.get_xAxis()
+    Signal_RF = Waveform(I.x0,I.dx,empty(time.shape[-1]))
+    ampBalance = IQMixer[0]
+    phaseBalance = IQMixer[1]
+    offsetI = IQMixer[2]
+    offsetQ = IQMixer[3]
+    mixed_I = (I.Y+offsetI)*cos( 2.*pi*LOFreq*time )
+    mixed_Q = (Q.Y+offsetQ)*ampBalance*cos( 2.*pi*LOFreq*time +radians(phaseBalance)) 
+    Signal_RF.Y = mixed_I+mixed_Q
+    return Signal_RF
 
 # API
 def get_Pulse_gauss ( duration:float, parameters:tuple, carrierFrequency:float=0, carrierPhase:float=0  ):
     """ 
-    If the carrier frequency of the pulse is 0 
+    Get a Pulse Object
     p0: Amplitude 
     p1: sigma
     p2: Peak Position
     """
-    newPulse = PulseBuilder()
+    newPulse = Pulse()
     newPulse.carrierFrequency = carrierFrequency
     newPulse.carrierPhase = carrierPhase
     newPulse.duration = duration
     newPulse.envelopeFunc = gaussianFunc
     newPulse.parameters = parameters
+
     return newPulse
 
 
+def get_Pulse_DRAG ( duration:float, parameters:tuple, carrierFrequency:float=0, carrierPhase:float=0  ):
+    """ 
+    Get a Pulse Object
+    p0: Amplitude 
+    p1: sigma
+    p2: Peak Position
+    """
+    newPulse = Pulse()
+    newPulse.carrierFrequency = carrierFrequency
+    newPulse.carrierPhase = carrierPhase
+    newPulse.duration = duration
+    newPulse.envelopeFunc = DRAGFunc
+    newPulse.parameters = parameters
 
+    return newPulse
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    a = get_Pulse_gauss( 10, ( 1, 4, 15 ), 10 )
-    a.generate_envelope( 10, 0.5 )
-    plt.plot( a.envelope.get_xAxis(), a.envelope.Y, "o")
+    IFFreq = -0.089
+    RFFreq = 5
+    LOFreq = RFFreq-IFFreq
+    MixerSetting = (1, -90, 0, 0) #IQMixer: tuple = (1, 90, 0, 0)
+    testPunse = get_Pulse_DRAG( 30, ( 1, 7.5, 0, 10 ), RFFreq, pi/2 )
+    WF_envelope = testPunse.generate_envelope( -15, 1 )
+
+    WF_signal = testPunse.generate_signal( -15, 0.001 )
+
+    WF_signal_I, WF_signal_Q = testPunse.generate_IQSignal( -15, 0.001, IFFreq, IQMixer=MixerSetting)
+
+    WF_signal_RF = simulate_IQMixer( WF_signal_I, WF_signal_Q, LOFreq, IQMixer=MixerSetting)
+
+    # Plot setting
+    fig, ax = plt.subplots(3,1,sharex=True)
+
+    # Compare signal and envelope
+    ax[0].plot( WF_envelope.get_xAxis(), abs(WF_envelope.Y), label="ABS envelope" )
+
+    ax[0].plot( WF_envelope.get_xAxis(), WF_envelope.Y.real, label="I envelope" )
+    ax[0].plot( WF_envelope.get_xAxis(), WF_envelope.Y.imag, label="Q envelope" )
+
+    ax[0].plot( WF_signal.get_xAxis(), WF_signal.Y, label="Target Signal" )
+    ax[0].legend()
+
+    # Compare IQ signal and envelope
+    ax[1].plot( WF_envelope.get_xAxis(), abs(WF_envelope.Y), label="envelope" )
+    ax[1].plot( WF_signal_I.get_xAxis(), WF_signal_I.Y, label="I" )
+    ax[1].plot( WF_signal_Q.get_xAxis(), WF_signal_Q.Y, label="Q" )
+    ax[1].legend()
+
+    # Compare Desire sigmal and remixed signal by IQ signal
+    ax[2].plot( WF_signal_RF.get_xAxis(), WF_signal_RF.Y, label="RF Signal" )
+    ax[2].plot( WF_signal.get_xAxis(), WF_signal.Y, label="Target Signal" )
+    ax[2].legend()
+
     plt.show()
+
